e2354649dbb0800238395784a65811e9c3d458dc
Isak Swearingen <isakswearingen@gmail.com>
2017-02-10 13:40:29 -0800

    quiz working

--- app.js
+++ app.js
@@ -1,46 +1,76 @@
 'use strict';
-// /*
+
+// question 1 - 5 correct variables
+var promtOne = ('Awesome, let\'s get started!');
+var correctOne = ('Of course he doesn\'t have a dog, dogs have mange and rabies.');
+var correctTwo = ('Wrong. Isak is disease free.');
+var correctThree = ('Yes, Isak practices good oral hygiene.');
+var correctFour = ('Yes, Isak has all of his toes.');
+var correctFive = ('Yes, but he has a tendency to crash them.');
+
+// question 1-5 incorrect variables
+var promtTwo = ('Too bad, we\'re doing this.');
+var wrongOne = ('Correct, he does not own a dog.');
+var wrongTwo = ('Correct, Isak does not have a disease.');
+var wrongThree = ('Wrong.');
+var wrongFour = ('Wrong, Isak has all of his toes.');
+var wrongFive = ('Wrong, Isak loves motorcycles.');
+
+//Questions 1-5
 var responseOne = confirm('Hello, would you like to take a quiz?');
 if(responseOne){
-  console.log('Awesome! Lets get started!');
+  console.log(promtOne);
+  alert(promtOne);
 }else{
-  console.log('Too bad, starting anyways.');
+  console.log(promtTwo);
+  alert(promtTwo);
 }
 
 var quizResponseOne = prompt('Please answer with Y or N.  Does Isak have a dog?');
 if(quizResponseOne.toUpperCase() === 'Y'){
-  console.log('Of course he does not have a dog.  Dogs have rabies.');
+  console.log(correctOne);
+  alert(correctOne);
 }else{
-  console.log('Yessir!');
+  console.log(wrongOne);
+  alert(wrongOne);
 };
 
 var quizResponseTwo = prompt('Please answer with Y or N.  Does Isak have a disease?');
 if(quizResponseTwo.toUpperCase() === 'Y'){
-  console.log('Ummm, no.');
+  console.log(correctTwo);
+  alert(correctTwo);
 }else{
-  console.log('Correct.  Isak is diesease free.');
+  console.log(wrongTwo);
+  alert(wrongTwo);
 }
 
 var quizResponseThree = prompt('Please answer with Y or N. Does Isak brush his teeth every day?');
 if(quizResponseThree.toUpperCase() === 'Y'){
-  console.log('Yes, Isak practices good oral hygiene.');
+  console.log(correctThree);
+  alert(correctThree);
 }else{
-  console.log('Wrong.');
+  console.log(wrongThree);
+  alert(wrongThree);
 }
 
-var quizResponseFour = prompt('Please answer with Y or N. Does Isak have a girlfriend?');
+var quizResponseFour = prompt('Does Isak have all of his toes?');
 if(quizResponseFour.toUpperCase() === 'Y'){
-  console.log('No. Isak is a single man.');
+  console.log(correctFour);
+  alert(correctFour);
 }else{
-  console.log('This is true.');
+  console.log(wrongFour);
+  alert(wrongFour);
 }
 
-var quizResponseFive = prompt('Please answer with Y or N. Is Isak good at coding?');
+var quizResponseFive = prompt('Does Isak ride motorcycles?');
 if(quizResponseFive.toUpperCase() === 'Y'){
-  console.log('Not yet he isn\'t.');
+  console.log(correctFive);
+  alert(correctFive);
 }else{
-  console.log('Well, technically that is true.');
+  console.log(wrongFive);
+  alert(wrongFive);
 }
+
 // //Question 6 variables
 var numberGuessCorrect = 'Wow, you actually got it!';
 var numberGuessHigh = 'Nope, too high.';
@@ -49,85 +79,50 @@ var guessHigh = (quizResponseSix >= 26);
 var guessLow = (quizResponseSix <= 24);
 var questionSeven;
 //
-// //for loop VERSION q 6
+// //for loop VERSION question 6
+//   //Ok, so quizResponseSix on line 54 equals a string.  to convert the string to a real number,use the parseInt fx. then redefine the variable to equal a real number.
 for(var i = 0; i < 4; i++){
   var quizResponseSix = prompt('Guess my favorite number. Hint: it\'s between 0 and 50.  You have 4 tries.');
-//   //Ok, so quizResponseSix on line 54 equals a string.  to convert the string to a real number,use the parseInt fx. then redefine the variable to equal a real number.
   quizResponseSix = parseInt(quizResponseSix, 10);
   if(quizResponseSix === 25){
     console.log(numberGuessCorrect);
+    alert(numberGuessCorrect);
     break;
   }else if(quizResponseSix >= 26) {
     console.log(numberGuessHigh);
+    alert(numberGuessHigh);
   }else if(quizResponseSix <= 24) {
     console.log(numberGuessLow);
+    alert(numberGuessLow);
   };
   if(i === 3){
     console.log('better luck next time!');
-  };
+  }
+};
 
-//
-// //ALL MY ORIGINAL BS Q 6 CODE.  DID IT AS AN ELSE IF INSTEAD OF A LOOP.
-// // question 6, 4 tries total
-// // var quizResponseSix = prompt('Guess my favorite number. Hint: it\'s between 0 and 50.  You have 4 tries.');
-// // quizResponseSix = parseInt(quizResponseSix, 10);
-// // if(quizResponseSix === 25){
-// //   console.log(numberGuessCorrect);
-// // }else if(quizResponseSix >= 26) {
-// //   console.log(numberGuessHigh);
-// // }else if(quizResponseSix <= 24) {
-// //   console.log(numberGuessLow);
-// // };
-//
-// // if (quizResponseSix !== 25){
-// //   var guessTwoWrong = prompt('3 tries left.');
-// // } if(guessTwoWrong === 25){
-// //   console.log(numberGuessCorrect) , (questionSeven);
-// // }else if(guessTwoWrong >= 26){
-// //   console.log(numberGuessHigh);
-// // }else if(guessTwoWrong <= 24){
-// //   console.log(numberGuessLow);
-// // };
-// //
-// // if (quizResponseSix !== 25){
-// //   var guessThreeWrong = prompt('Only 2 tries left.');
-// // } if(guessThreeWrong === 25){
-// //   console.log(numberGuessCorrect) , (questionSeven);
-// // }else if(guessThreeWrong >= 26){
-// //   console.log(numberGuessHigh);
-// // }else if(guessThreeWrong <= 24){
-// //   console.log(numberGuessLow);
-// // };
-// //
-// // if (quizResponseSix !== 25){
-// //   var guessFourWrong = prompt('Look, I don\'t want to freak you out or anything, but if you don\'t get this last guess right, terrible things will happen.');
-// // }if(guessFourWrong === 25) {
-// //   console.log(numberGuessCorrect), (questionSeven);
-// // }else{
-// //   console.log('Ya blew it.  It ws 25, obviously.  Seriously, what is wrong with you?!');
-// // };
-// // //End of question 6
-
-// //q'n 7 variables
+// //question 7 variables
 var states = ['Nevada', 'Georgia', 'North Carolina'];
 var arrayLength = states.length;
 var maximumTries = 6;
 var questionSeven;
 var isCorrect = false;
 
+//question 7
+
 for(var tries = 0; tries < maximumTries; tries++){
-questionSeven = prompt('Try to guess a state I have lived in other than Washington');
+  questionSeven = prompt('Try to guess a state I have lived in other than Washington');
   for(var i = 0; i < states.length; i++){
-    if(questionSeven === states[i]){
+    if(questionSeven === states.toUpperCase()[i]){
       isCorrect = true;
       break;
     }
   }
   if(isCorrect === true){
-    console.log('Good work');
+    console.log('Good work!');
+    alert('Good Work!');
     break;
   }else{
-    console.log('keep trying');
+    console.log('Keep Trying');
+    alert('Wrong.');
   }
-}
 };
